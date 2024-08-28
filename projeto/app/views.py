fd8c97be480ed045df3f5fbc60ac25908a569d4f
LUCIANO COSME DA SILVA <luciano.csilva15@senacsp.edu.br>
2024-08-28 19:41:51 -0300

    28/07/2024 - Commit inicial do projeto

---
+++ projeto/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Tarefa
+from .forms import TarefaForm
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all()
+    return render(request, 'html/listar_tarefas.html', {'lista_tarefas': tarefas})
+
+def criar_tarefa(request):
+    if request.method == "POST":
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rotaTarefas')
+    else:
+        form = TarefaForm()
+
+    return render(request, 'html/form_tarefa.html', {'form': form})
