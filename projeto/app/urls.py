1719990a9bb75c0538939bc111d0da8e17732c62
ANTONIO GABRIEL CARDOSO DE REZENDE ORLANDINI <antonio.gcrorlandini@senacsp.edu.br>
2024-08-28 22:10:09 -0300

    28/08/2024 - Crud completo de tarefas

--- projeto/app/urls.py
+++ projeto/app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import listar_tarefas, criar_tarefa
+from .views import listar_tarefas, criar_tarefa, editar_tarefa, excluir_tarefa
 
 urlpatterns = [
     path('', listar_tarefas, name='listar_tarefas'),
     path('nova/', criar_tarefa, name='rotaNovaTarefa'),
-    #path('editar/', editar_tarefa, name='rotaEditarTarefa'),
+    path('editar/<int:id>/', editar_tarefa, name='rotaEditarTarefa'),
+    path('excluir/<int:id>/', excluir_tarefa, name='rotaExcluirTarefa'),
 ]
--- projeto/app/views.py
+++ projeto/app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefa
 from .forms import TarefaForm
 
 def listar_tarefas(request):
-  lista_tarefas = Tarefa.objects.all()
+  # Caso queira ordenar: lista_tarefas = Tarefa.objects.order_by("id_tarefa")
+  lista_tarefas = Tarefa.objects.all().order_by('id_tarefa')
 
   return render(request, 'html/listar_tarefas.html', {'lista_tarefas': lista_tarefas})
 
@@ -19,4 +20,29 @@ def criar_tarefa(request):
   
   return render(request, 'html/form_tarefa.html', {'form': form})
 
-# def editar_tarefa():
+# Esta view recebe o parâmetro "id" que foi definido na rota
+def editar_tarefa(request, id):
+
+  # Buscar a tarefa onde o "id_tarefa" seja igual ao parâmetro "id" 
+  tarefa = get_object_or_404(Tarefa, id_tarefa = id)
+  
+  if request.method == "POST":
+    form = TarefaForm(request.POST, instance=tarefa)
+    if form.is_valid():
+      form.save()
+      return redirect('listar_tarefas')
+  
+  else:
+    form = TarefaForm(instance=tarefa)
+  
+  return render(request, 'html/form_tarefa.html', {'form': form})
+
+def excluir_tarefa(request, id):
+  # Buscar a tarefa onde o "id_tarefa" seja igual ao parâmetro "id" 
+  tarefa = get_object_or_404(Tarefa, id_tarefa = id)
+  
+  if request.method == "POST":
+    tarefa.delete()
+    return redirect('listar_tarefas')
+  
+  return render(request, 'html/excluir_tarefa.html', {'tarefa': tarefa})
