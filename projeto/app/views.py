135caf501adb0687b18655a744178d5f09fedfa7
ELVIS APARECIDO DE SOUZA <elvis.asouza@senacsp.edu.br>
2024-08-28 22:09:33 -0300

    28/08/2024 - conteudo das atividades

--- projeto/app/views.py
+++ projeto/app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tarefa
 from .forms import TarefaForm
 
 def listar_tarefas(request):
-    tarefas = Tarefa.objects.all()
+    tarefas = Tarefa.objects.all() .order_by('id_tarefa')
     return render(request, 'html/listar_tarefas.html', {'lista_tarefas': tarefas})
 
 def criar_tarefa(request):
@@ -16,3 +16,34 @@ def criar_tarefa(request):
         form = TarefaForm()
 
     return render(request, 'html/form_tarefa.html', {'form': form})
+
+# Esta view recebe o parametro "id" que foi definido na rota
+def editar_tarefa(request, id):
+
+    # Buscar a tarefa onde o id_tarefa seja igual ao parametro "id"
+    tarefa = get_object_or_404(Tarefa, id_tarefa=id)
+    
+    if request.method == "POST":
+        form = TarefaForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            return redirect('rotaTarefas')
+    else:
+        form = TarefaForm(instance=tarefa)
+
+    return render(request, 'html/form_tarefa.html', {'form': form})
+
+
+def excluir_tarefa(request, id):
+
+    # Buscar a tarefa onde o id_tarefa seja igual ao parametro "id"
+    tarefa = get_object_or_404(Tarefa, id_tarefa=id)
+    
+    if request.method == "POST":
+        tarefa.delete()
+        return redirect('rotaTarefas')
+
+    return render(request, 'html/excluir_tarefa.html', {'tarefa': tarefa})
+
+
+
