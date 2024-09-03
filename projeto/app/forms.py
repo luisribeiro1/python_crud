f8bb3095e0b9bdcc2bbf4ce1286fd8538eccbfce
RICARDO CARLOS FEITOSA <ricardo.cfeitosa@senacsp.edu.br>
2024-09-02 22:22:39 -0300

    02/09 -Projeto Python CRUD

---
+++ projeto/app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Tarefa
+
+class TarefaForm(forms.ModelForm):
+    class Meta:
+        model = Tarefa
+        fields = ['titulo', 'prazo', 'responsavel', 'status']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'prazo': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            # 'responsavel': forms.TextInput(attrs={'class': 'form-control'}),
+            'responsavel': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class UsuariosForm(forms.ModelForm):
+    class Meta:
+        model = Usuarios
+        fields = ['', 'prazo', 'responsavel', 'status']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'prazo': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            # 'responsavel': forms.TextInput(attrs={'class': 'form-control'}),
+            'responsavel': forms.Select(attrs={'class': 'form-select'}),
+        }
---
+++ projeto/app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100 )
+
+    class Meta:
+        db_table = 'usuarios'
+
+    def __str__(self):
+        return self.nome
+
+
+class Tarefa(models.Model):
+    id_tarefa = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    prazo = models.DateField()
+    # responsavel = models.CharField(max_length=100)
+    responsavel = models.ForeignKey(
+        Usuario, 
+        on_delete=models.CASCADE,
+        db_column='responsavel')
+    status = models.BooleanField()
+
+    class Meta:
+        db_table = 'tarefas2'
+
