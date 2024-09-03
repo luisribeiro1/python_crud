5623739b30cfe009e1a55ac62fd3d6bd366ce093
Antonio Rezende <antoniororlandini@gmail.com>
2024-09-02 22:26:15 -0300

    02/09/2024 - Usuarios Crud

--- projeto/app/forms.py
+++ projeto/app/forms.py
@@ -6,8 +6,7 @@ class TarefaForm(forms.ModelForm):
         model = Tarefa
         fields = ['titulo', 'prazo', 'responsavel', 'status']
         widgets = {
-            'titulo': forms.TextInput(attrs={'class':'form-control'}),
-            'prazo': forms.DateInput(attrs={'type':'date', 'class':'form-control'}),
-            'responsavel': forms.TextInput(attrs={'class':'form-control'}),
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'prazo': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'responsavel': forms.Select(attrs={'class': 'form-select'}),
         }
-
--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import listar_tarefas, criar_tarefa
+from app.views import listar_tarefas, criar_tarefa, editar_tarefa, excluir_tarefa
 
 urlpatterns = [
     path('', listar_tarefas, name='rotaTarefas'),
     path('nova/', criar_tarefa, name='rotaNovaTarefa'),
-    # path('editar/', editar_tarefa, name='rotaEditarTarefa'),
-
+    path('editar/<int:id>/', editar_tarefa, name='rotaEditarTarefa'),
+    path('excluir/<int:id>/', excluir_tarefa, name='rotaExcluirTarefa'),
+    path('usuarios/', listar_usuarios, name='rotaUsuarios')
 ]
